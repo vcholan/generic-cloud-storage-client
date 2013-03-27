d31e99e5c52b5978d10f58a6d713ece8003cf295
Riju Vashisht <rijuvashisht@gmail.com>
2013-03-26 22:43:32 -0400

    Selection for the cloud service provider.
    Enum type are used for service providers.

---
+++ generic cloud storage client/ServiceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic_cloud_storage_client
+{
+    public enum ServiceProvider
+    {
+        GoogleCloud,OpenStack,RackSpace,WindowsAzure
+    }
+}
